19fbff5e08f0088f3cac07976aa00e7ece0328d5
Peter Armstrong <peter@ruboss.com>
2012-10-09 15:33:46 -0700

    only support reviewed locales

--- copy_from_leanpub.sh
+++ copy_from_leanpub.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-rm *.yml
+rm -f *.yml
+rm -f unsupported_locales/*.yml
 cp -r ~/lpub/config/locales/*.yml .
+cp -r ~/lpub/config/locales/unsupported_locales/*.yml unsupported_locales
--- copy_to_leanpub.sh
+++ copy_to_leanpub.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
-rm ~/lpub/config/locales/*.yml
+rm -f ~/lpub/config/locales/*.yml
+rm -f ~/lpub/config/locales/unsupported_locales/*.yml
 cp -r *.yml ~/lpub/config/locales
+cp -r unsupported_locales/*.yml ~/lpub/config/locales/unsupported_locales
