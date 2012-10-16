5d091ff0bd8c69005d883f7157818d6d9a337840
Peter Armstrong <peter@ruboss.com>
2012-10-15 23:14:34 -0700

    support levels

--- copy_from_leanpub.sh
+++ copy_from_leanpub.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 rm -f *.yml
-rm -f unsupported_locales/*.yml
 cp -r ~/lpub/config/locales/*.yml .
-cp -r ~/lpub/config/locales/unsupported_locales/*.yml unsupported_locales
+
--- copy_to_leanpub.sh
+++ copy_to_leanpub.sh
@@ -1,5 +1,3 @@
 #!/bin/sh
 rm -f ~/lpub/config/locales/*.yml
-rm -f ~/lpub/config/locales/unsupported_locales/*.yml
 cp -r *.yml ~/lpub/config/locales
-cp -r unsupported_locales/*.yml ~/lpub/config/locales/unsupported_locales
